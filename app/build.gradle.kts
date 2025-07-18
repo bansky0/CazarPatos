905167ba8a12dabc6271723e689a794b8fdd1168
bansky0 <bansky0@gmail.com>
2025-07-18 14:05:49 -0500

    Ranking SQLite

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.jhonatan.lopez.cazarpatos"
-        minSdk = 24
+        minSdk = 28
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
--- app/src/main/java/com/jhonatan/lopez/cazarpatos/RankingActivity.kt
+++ app/src/main/java/com/jhonatan/lopez/cazarpatos/RankingActivity.kt
@@ -7,7 +7,49 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.jhonatan.lopez.cazarpatos.database.RankingPlayerDBHelper
 
+class RankingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ranking)
+
+        OperacionesSqLite()
+        GrabarRankingSQLite()
+        LeerRankingsSQLite()
+    }
+
+    fun OperacionesSqLite(){
+        RankingPlayerDBHelper(this).deleteAllRanking()
+        RankingPlayerDBHelper(this).insertRankingByQuery(Player("Jugador9",10))
+        val patosCazados = RankingPlayerDBHelper(this).readDucksHuntedByPlayer("Jugador9")
+        RankingPlayerDBHelper(this).updateRanking(Player("Jugador9",5))
+        RankingPlayerDBHelper(this).deleteRanking("Jugador9")
+        RankingPlayerDBHelper(this).insertRanking(Player("Jugador9",7))
+        val players = RankingPlayerDBHelper(this).readAllRankingByQuery()
+    }
+    fun GrabarRankingSQLite(){
+        val jugadores = arrayListOf(
+            Player("Jhonatan.Lopez", 10),
+            Player("Jugador2", 6),
+            Player("Jugador3", 3),
+            Player("Jugador4", 9)
+        )
+        jugadores.sortByDescending { it.huntedDucks }
+        for(jugador in jugadores){
+            RankingPlayerDBHelper(this).insertRanking(jugador)
+        }
+    }
+    fun LeerRankingsSQLite(){
+        val jugadoresSQLite = RankingPlayerDBHelper(this).readAllRanking()
+        val recyclerViewRanking: RecyclerView = findViewById(R.id.recyclerViewRanking)
+        recyclerViewRanking.layoutManager = LinearLayoutManager(this)
+        recyclerViewRanking.adapter = RankingAdapter(jugadoresSQLite)
+        recyclerViewRanking.setHasFixedSize(true)
+    }
+}
+
+/*
 class RankingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,3 +75,4 @@ class RankingActivity : AppCompatActivity() {
 
     }
 }
+*/
